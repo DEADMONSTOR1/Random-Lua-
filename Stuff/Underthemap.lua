b2c7382cdd1b443f15e3a1e733d4e027082af7e7
DEADMONSTOR <josh14m@aol.com>
2016-04-12 00:25:37 +0100

    Update Underthemap.lua

--- Stuff/Underthemap.lua
+++ Stuff/Underthemap.lua
@@ -1,14 +1,17 @@
+
 Local underthemap = {
-          min = Vector(0,0,-40)
+          min = Vector(0,0,-40),
           max = Vector(300,300,0)
 }
+
 local box = ents.FindInBox(underthemap.min,underthemap.max)
+
 Function mapchecker()
-for k,v in pairs(box) do
-if v:GetClass() == "Nukething" then
- Local pos = v:GetPos()
- v:SetPos(pos + Vector(0,0,45)
+     for k,v in pairs(box) do
+          if v:GetClass() == "Nukething" then
+               Local pos = v:GetPos()
+               v:SetPos(pos + Vector(0,0,45)
+          end
+     end
 end
-end
-end
-Timer.Create("check map", 10,0,mapchecker)
+Timer.Create("check map" , 10, 0, mapchecker)
