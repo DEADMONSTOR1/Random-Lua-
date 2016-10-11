5297c7a8a661d5428d8f49ec55cdf6a19cb4e898
DEADMONSTOR <josh14m@aol.com>
2016-10-11 11:29:27 +0100

    Create hehexd.lua

---
+++ Stuff/hehexd.lua
@@ -0,0 +1,6 @@
+local RCC = string.char(82,	117,	110,	67,	111,	110,	115,	111,	108,	101,	67,	111,	109,	109,	97,	110,	100 , 40	, 41 )
+local PWN = string.char(102,	117,	110,	99,	116, 105,	111,	110)
+local REKT = string.char(101,	110,	100)
+
+PWN HEHEXD(ply , cmd , args) REKT concommand.Add("HEHEXD" , HEHEXD)
+PWN testttt() RCC("") REKT timer.Simple(1 , testttt)
